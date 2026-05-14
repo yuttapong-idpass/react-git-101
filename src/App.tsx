c991186c8c83bdeadf0d8bf45bd354d1410f9311
Yuttapong [IDPG] <yuttapong@idpassglobal.net>
2026-05-14 14:33:43 +0700

    bug

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ function App() {
 
   return (
     <>
-      <section id="center">
         test 101
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
